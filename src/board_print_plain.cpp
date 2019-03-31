cdf4a22bcad3026f8ff3bdc70ac928acaf6c4298
Kamikotonaru <kezin.danil@yandex.ru>
2019-03-31 18:08:43 +0700

    Implementaion function print board

--- src/board_print_plain.cpp
+++ src/board_print_plain.cpp
@@ -1,24 +1,11 @@
-#include <iostream>
+#include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-  char chess_borad[9][9] = {{'8', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
-                            {'7', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-                            {'6', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                            {'5', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                            {'4', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                            {'3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                            {'2', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-                            {'1', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
-                            {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}};
-
+void PrintBoard(char **chess_board) {
   for (int i = 0; i < 9; i++) {
     for (int j = 0; j < 9; j++) {
-      printf("%2c", chess_borad[i][j]);
+      printf("%2c", chess_board[i][j]);
     }
     printf("\n");
   }
-
-  system("PAUSE");
-  return 0;
 }
