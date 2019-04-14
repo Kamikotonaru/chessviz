c1188f6c4005363ea060c0ea77c532d986f07445
Kamikotonaru <kezin.danil@yandex.ru>
2019-04-14 17:53:49 +0700

    Implementation of partitioning the input symbols

---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+	char chess_borad[9][9] = {{'8', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+                            {'7', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                            {'6', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                            {'5', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                            {'4', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                            {'3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                            {'2', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                            {'1', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+                            {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}};
+
+	char step[80];
+	printf("Input step\n");
+	puts(step);
+  
+	char *stepBlack = strtok(step, " ");
+    char *stepWhite = stepBlack;
+	stepBlack = strtok(NULL," ");
+	
+	char *thereWhite = strtok(stepWhite,"-");
+	char *hereWhite = thereWhite;
+	thereWhite = strtok(NULL," ");
+	
+	char *thereBlack = strtok(stepBlack,"-");
+	char *hereBlack = thereBlack;
+	thereBlack = strtok(NULL," ");
+	
+	char hereIntW[3];
+	char thereIntW[3];
+	char hereIntB[3];
+	char thereIntB[3];
+	
+	strcpy(hereIntW,hereWhite);
+	strcpy(thereIntW,thereWhite);
+	strcpy(hereIntB,hereBlack);
+	strcpy(thereIntB,thereBlack);
+	
+	char *charHereW = strtok(hereWhite,"12345678");
+	char *intHereW = strtok(hereIntW,"abcdefgh");
+	
+	char *charThereW = strtok(thereWhite,"12345678");
+	char *intThereW = strtok(thereIntW,"abcdefgh");
+	
+	char *charHereB = strtok(hereBlack,"12345678");
+	char *intHereB = strtok(hereIntB,"abcdefgh");
+
+	char *charThereB = strtok(thereBlack,"12345678");
+	char *intThereB = strtok(thereIntB,"abcdefgh");
+	
+	return 0;
+}
