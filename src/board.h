4dedc97fc040e3017efcaf033f206f6e354d69af
Kamikotonaru <kezin.danil@yandex.ru>
2019-04-14 19:31:33 +0700

    Description function CheckInput

--- src/board.h
+++ src/board.h
@@ -1,6 +1,8 @@
 #ifndef BOARD_H
 #define BOARD_H 
+#include "board.cpp"
 
+int CheckInput(char *steps);
 void Move(char *charHereW, char* intHereW, char *charThereW, char *intThereW, char *charHereB, char *intHereB, char *charThereB, char *intThereB, char (*chessB)[9]);
 
 #endif
