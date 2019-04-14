30b735be7a41972f8086f1123d7937b206fb52db
Kamikotonaru <kezin.danil@yandex.ru>
2019-04-14 18:09:57 +0700

    Description functions:board.h->Move(),b_p_p.h->PrintBoard

---
+++ src/board.h
@@ -0,0 +1,6 @@
+#ifndef BOARD_H
+#define BOARD_H 
+
+void Move(char *charHereW, char* intHereW, char *charThereW, char *intThereW, char *charHereB, char *intHereB, char *charThereB, char *intThereB, char (*chessB)[9]);
+
+#endif
